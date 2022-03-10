f037d2d2ec2a1b23e6733245dd6edbc747910bd4
Guilherme <guilhermeevencioluz@gmail.com>
2022-03-10 18:09:04 -0300

    Implementa a função getProductsFromCategoryAndQuery, cumprindo o que foi pedido no requisisto 01.

--- src/services/api.js
+++ src/services/api.js
@@ -2,10 +2,12 @@ export async function getCategories() {
   // Implemente aqui
   const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
   const data = await response.json();
-
   return data;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`);
+  const data = await response.json();
+  return data;
 }
