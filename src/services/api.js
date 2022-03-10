2eff06c259569dea2c6abb57f6caa5a5793a1d69
jose-cleiton <cleitons835@gmail.com>
2022-03-10 16:22:54 -0300

    Primeiro Requisito

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,9 @@
 export async function getCategories() {
   // Implemente aqui
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const data = await response.json();
+
+  return data;
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
