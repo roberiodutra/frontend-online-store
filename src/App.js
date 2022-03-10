4ce6982c76fe5822d072e69e35fd6a3718c74a23
Davi Freitas <df828316@gmail.com>
2022-03-10 18:40:18 -0300

    requisito 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import Search from './pages/Search';
+import Carrinho from './pages/Carrinho';
 
 function App() {
   return (
     <Router>
+      <Link to="/carrinho" data-testid="shopping-cart-button">Carrinho</Link>
       <Switch>
         <Route exact path="/" component={ Search } />
+        <Route exact path="/carrinho" component={ Carrinho } />
       </Switch>
     </Router>
   );
---
+++ src/pages/Carrinho.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Carrinho extends Component {
+  render() {
+    return (
+      <div>
+        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+      </div>
+    );
+  }
+}
+
+export default Carrinho;
